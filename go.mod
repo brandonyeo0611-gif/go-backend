585b8ee56bc620ecad6bd0c1245825c07a545453
Brandon <brandonyeo0611@gmail.com>
2025-12-23 22:49:59 +0800

    create middleware... need read up on it more

--- go.mod
+++ go.mod
@@ -4,11 +4,8 @@ go 1.21
 
 require (
 	github.com/go-chi/chi/v5 v5.0.10
+	github.com/go-chi/cors v1.2.2
+	github.com/golang-jwt/jwt/v5 v5.3.0
+	github.com/lib/pq v1.10.9
 	github.com/pkg/errors v0.9.1
-)
-
-require (
-	github.com/go-chi/cors v1.2.2 // indirect
-	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
 )
--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,8 +1,38 @@
 package middleware
 
-import {
+import (
 	"net/http"
 	"strings"
 	"context"
+
 	"github.com/CVWO/sample-go-app/internal/auth"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, `{"error":"Authorization header required"}`, 
+		http.StatusUnauthorized)
+			return
+		}
+
+		tokenStr := strings.Replace(authHeader, "Bearer ", "", 1) 
+		// to get the data
+
+		// to validate the token
+		claims, err := auth.ValidateToken(tokenStr)
+		if err != nil {
+			http.Error(w, `{"error":"Invalid token"}`, http.StatusUnauthorized)
+		return
+
+		}
+
+		// pass user to handler via context
+		ctx := context.WithValue(r.Context(), "user_id", claims.UserID)
+		ctx = context.WithValue(ctx, "username", claims.Username)
+
+		next.ServeHTTP(w,r.WithContext(ctx))
+	})
 }
